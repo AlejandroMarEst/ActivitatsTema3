4f3170f0cb98f1aaafb7bd6406efa7e51416638c
AlejandroMarEst <alejandro.martin.7e8@itb.cat>
2024-12-09 11:50:54 +0100

    Finished Exercici 1 Closes #1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+namespace ActivitatsTema3
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Player Messi = new Player("Messi", 50);
+            Team Barca = new Team("Barca");
+            Console.WriteLine(Messi.Name);
+            Console.WriteLine(Barca.TeamName);
+        }
+    }
+}
---
+++ Team.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ActivitatsTema3
+{
+    public class Team
+    {
+        // Formado por un conjunto de jugadores (Lista)
+        public string TeamName { get; set; }
+        public Team(string name)
+        {
+            TeamName = name;
+        }
+    }
+}
