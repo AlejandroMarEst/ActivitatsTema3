462e15f1a06ee115ac90de1c9377c7df7cca8682
AlejandroMarEst <alejandro.martin.7e8@itb.cat>
2024-12-10 13:55:48 +0100

    Finished Activitat 6 Closes  #7

---
+++ Card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ActivitatsTema3
+{
+    public class Card
+    {
+        public string Type { get; set; }
+
+        public int Value { get; set; }
+
+        public Card(string type, int value)
+        {
+            this.Type = type;
+            this.Value = value;
+        }
+    }
+}
