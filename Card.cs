427c37b746ed7e50652200dc415f5b0e735f8d6a
AlejandroMarEst <alejandro.martin.7e8@itb.cat>
2024-12-11 12:03:49 +0100

    Finished activitat 6 Closes #13

---
+++ Card.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace ActivitatsTema3
+{
+    public class Card : TabletopGame
+    {
+        public string Type { get; set; }
+
+        public int Value { get; set; }
+        public Card(string type, int value, string name, int players) : base(name, players)
+        {
+            Type = type;
+            Value = value;
+        }
+    }
+}
---
+++ Lamp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ActivitatsTema3
+{
+    public class Lamp : Furniture
+    {
+        public int Power { get; set; }
+
+        public Lamp(string color, int power, int height, int width) : base (color, height, width)
+        {
+            Power = power;
+        }
+    }
+}
