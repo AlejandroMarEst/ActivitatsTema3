f4ac6c46c088be7bda1bb5892c2bdb948bb8c386
AlejandroMarEst <alejandro.martin.7e8@itb.cat>
2024-12-12 13:51:28 +0100

    Finished Exercici 16 Closes #22

---
+++ Library.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivitatsTema3
+{
+    public class Library
+    {
+        // No es pot fer la relacio ja que es necessari una llista de llibres
+        public string Name { get; set; }
+        public string Ubicacio { get; set; }
+        public Library(string name, string ubicacio)
+        {
+            Name = name;
+            Ubicacio = ubicacio;
+        }
+    }
+}
