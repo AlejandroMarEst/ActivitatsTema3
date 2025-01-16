a0a006ddfa4974777bcc2010f88accd8d77a366f
AlejandroMarEst <alejandro.martin.7e8@itb.cat>
2025-01-16 13:04:24 +0100

    Finished Activitat 18 Closes #29

---
+++ Ikea.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivitatsTema3
+{
+    public class Ikea
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string? Description { get; set; }
+        public Ikea(string name, double price, string? description)
+        {
+            Name = name;
+            Price = price;
+            Description = description;
+        }
+    }
+}
